cea3332fb99ecec21ba32479a407e507963c6fd0
Tejas <tejasbarmase777@gmail.com>
2024-08-31 02:53:19 +0530

    Update game.js

--- game.js
+++ game.js
@@ -1,8 +1,15 @@
 var buttonColours = ["red", "blue", "green", "yellow"];
 
+var gamePattern = [];
 
-function nextSequence(){
-    var randomNumber = Math.floor(Math.random()*3 + 1);
+function nextSequence() {
+    
+    var randomNumber = Math.floor(Math.random()*4);
+    
+    var randomChosenColour = buttonColours[randomNumber];
+
+    gamePattern,push(randomChosenColour);
+
 }
 
 
